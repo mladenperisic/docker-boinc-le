9ea9cbfa1e1f22ef05d29b440079beb55322aec3
mladenperisic <mladen.perisic84@gmail.com>
2017-01-18 00:30:18 +0100

    Update firstrun.sh
    1) Updated to latest BOINC Linux version
    2) Added option for allowing remote managment GUI

--- firstrun.sh
+++ firstrun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install boinc client and manager
-cp /nobody/boinc/boinc_7.2.42_x86_64-pc-linux-gnu.sh /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
-chmod +x /config/boinc_7.2.42_x86_64-pc-linux-gnu.sh
+cp /nobody/boinc/boinc_7.4.25_x86_64-pc-linux-gnu.sh /config/boinc_7.4.25_x86_64-pc-linux-gnu.sh
+chmod +x /config/boinc_7.4.25_x86_64-pc-linux-gnu.sh
 chown -R nobody:users /config
 su -c "cd /config && ./boinc_7.2.42_x86_64-pc-linux-gnu.sh" -s /bin/sh nobody
-su -c "/config/BOINC/run_client --daemon" -s /bin/sh nobody
+su -c "/config/BOINC/run_client --daemon --allow_remote_gui_rpc" -s /bin/sh nobody
